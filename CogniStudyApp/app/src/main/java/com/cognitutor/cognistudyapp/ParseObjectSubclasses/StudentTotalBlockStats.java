4cebe86cd1a4b7a098d20db8abb84d1422cf885d
Kevin Joslyn <KJoslyn29@gmail.com>
2016-02-29 12:39:35 -0500

    Added StudentTotalBlockStats

---
+++ CogniStudyApp/app/src/main/java/com/cognitutor/cognistudyapp/ParseObjectSubclasses/StudentTotalBlockStats.java
@@ -0,0 +1,15 @@
+package com.cognitutor.cognistudyapp.ParseObjectSubclasses;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+/**
+ * Created by Kevin on 2/29/2016.
+ */
+public abstract class StudentTotalBlockStats extends StudentBlockStats {
+
+    @Override
+    public void setSubjectOrCategory(String category) {
+        //N/A
+    }
+}
